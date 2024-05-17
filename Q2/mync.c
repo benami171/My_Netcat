77d26b38e66d32e837e41f10e4afafa9a0876d79
chanan-hash <chananhelman@gmail.com>
2024-05-17 15:36:25 +0300

    working on mync

--- Q2/mync.c
+++ Q2/mync.c
@@ -4,13 +4,13 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3 || strcmp(argv[1], "-e") != 0)
+    if (argc != 4 || strcmp(argv[1], "-e") != 0)
     {
         printf("Usage: %s -e <program>\n", argv[0]);
         return 1;
     }
 
-    char *new_argv[] = {argv[2], NULL};
+    char *new_argv[] = {argv[2], argv[3], NULL};
     execv(argv[2], new_argv);
 
     // If execv returns, there was an error
