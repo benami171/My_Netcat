427192f93f9429a4301a2e3c6e8e4294dd6ebfc9
chanan-hash <chananhelman@gmail.com>
2024-05-20 09:53:40 +0300

    Update

--- Q2/mync.c
+++ Q2/mync.c
@@ -1,67 +1,9 @@
-// #include <stdio.h>
-// #include <unistd.h>
-// #include <string.h>
-
-// int main(int argc, char *argv[])
-// {
-//     if (argc != 4 || strcmp(argv[1], "-e") != 0)
-//     {
-//         printf("Usage: %s -e <program>\n", argv[0]);
-//         return 1;
-//     }
-
-//     char *new_argv[] = {argv[2], argv[3], NULL};
-//     execv(argv[2], new_argv);
-
-//     // If execv returns, there was an error
-//     perror("execv");
-//     return 1;
-// }
-
-// #include <stdio.h>
-// #include <unistd.h>
-// #include <string.h>
-// #include <sys/wait.h>
-
-// int main(int argc, char *argv[])
-// {
-//     if (argc != 4 || strcmp(argv[1], "-e") != 0)
-//     {
-//         printf("Usage: %s -e <program>\n", argv[0]);
-//         return 1;
-//     }
-
-//     char *new_argv[] = {argv[2], argv[3], NULL};
-
-//     pid_t pid = fork();
-
-//     if (pid < 0)
-//     {
-//         perror("fork");
-//         return 1;
-//     }
-//     else if (pid == 0)
-//     {
-//         // Child process
-//         execv(argv[2], new_argv);
-
-//         // If execv returns, there was an error
-//         perror("execv");
-//         return 1;
-//     }
-//     else
-//     {
-//         // Parent process
-//         int status;
-//         waitpid(pid, &status, 0);
-//     }
-
-//     return 0;
-// }
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 
 int main(int argc, char *argv[])
 {
